fb0c7c8083c74f69490790632055ee14dacadef9
Testimony Adekoya <hardeykoryar7@gmail.com>
2024-05-02 14:11:32 +0100

    Restructuring code with classes

--- src/db_create.py
+++ src/db_create.py
@@ -3,20 +3,24 @@ from langchain.embeddings.openai import OpenAIEmbeddings
 from langchain.vectorstores import chroma
 
 
-# creating vector database for Mental Health Knowledge base
-def create_db(pdf_data, db_name):
-    # creating vector database
+class VectorDatabase:
 
-    persist_directory = os.path.join("../vector_embedding", db_name)
+    def __init__(self, db_name):
+        self.db_name = db_name
+        self.persist_directory = os.path.join("../vector_embedding", db_name)  # creating vector database
+        self.openai_key = os.environ.get("OPENAI_API_KEY")
+        self.embeddings = OpenAIEmbeddings(openai_api_key=openai_key)
 
-    openai_key = os.environ.get("OPENAI_API_KEY")
-    embeddings = OpenAIEmbeddings(openai_api_key=openai_key)
-    vectDB = chroma.Chroma.from_documents(pdf_data, embeddings,
-                                          collection_name= db_name,
-                                          persist_directory=persist_directory
-                                          )
-    
-    vectDB.persist()
-    
-    
 
+
+
+    # creating vector database for Mental Health Knowledge base
+    def create_db(self, pdf_data):       
+        self.vectDB = chroma.Chroma.from_documents(pdf_data,self.embeddings,
+                                            collection_name= self.db_name,
+                                            persist_directory=self.persist_directory
+                                            )
+        self.vectDB.persist()
+
+    def load_db(self):
+        self.vectDB = chroma.Chroma.load(self.persist_directory)
