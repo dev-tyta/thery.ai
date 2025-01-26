69a9db6e65891089c82972b09140da135c8778ae
Testimony Adekoya <hardeykoryar7@gmail.com>
2025-01-26 22:05:12 +0100

    WIP: fixing in the necessary TODO to carry out before resumption of work in 2/3 hours

--- src/spot_music/fetch.py
+++ src/spot_music/fetch.py
@@ -1,1 +1,2 @@
-
+#TODO:Work on the fetching music from Spotify. Figure out a way to find music based on the emotion detected during the conversation.
+#TODO: Implement a smooth algorithm to sync music without a fail.
---
+++ src/tele_bot/bot.py
@@ -0,0 +1,1 @@
+#TODO: Collate the necessary features for the Telegram bot, including parts like buttons, other services, Web Search Feature (*Might be nice to try this out.*)
